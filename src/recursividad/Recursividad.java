6ef35e0204d371fa1338b4b761fcc283006beb74
David191309 <25030698@itesa.edu.mx>
2026-09-13 21:47:13 -0600

    Version_02: Recursividades

--- src/recursividad/Recursividad.java
+++ src/recursividad/Recursividad.java
@@ -1,20 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package recursividad;
 
-/**
- *
- * @author david
- */
 public class Recursividad {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static long factorial(int n) {
+
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
     }
-    
+
+    public static void main(String[] args) {
+
+        System.out.println("Factorial de 5: " + factorial(5));
+
+    }
 }
